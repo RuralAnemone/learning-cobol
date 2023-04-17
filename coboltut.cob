0e448e964535572e9b9288553ef307dcb7a9df0a
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-04-17 23:43:16 +0000

    indent

--- coboltut.cob
+++ coboltut.cob
@@ -1,23 +1,25 @@
        >>source format free
-identification division.
-program-id. coboltut.
-author. Isaiah Wallerstedt.
-date-written.April 17th 2023
-environment division.
-
-data division.
-file section.
-
-* probably just gonna get familiar with the syntax highlighting for now lol
-working-storage section.
-01 UserName    pic x(30) value "user0".
-01 Num1    pic 9 value zeroes.
-01 Num2    pic 9 value zeroes.
-01 Total   pic 99 value 0.
-01 SSNum.
-       02 SSArea   pic 999.
-       02 SSGroup  pic 99.
-       02 SSSerial pic 9999.
-01 PIValue constant as 3.14
-
-procedure division.
+       identification division.
+       program-id. coboltut.
+       author. Isaiah Wallerstedt.
+       date-written. April 17th 2023.
+       
+       environment division.
+       
+       data division.
+       file section.
+       
+      * probably just gonna get familiar with the syntax highlighting for now lol
+       working-storage section.
+       01 UserName    pic x(30) value "user0".
+       01 Num1    pic 9 value zeroes.
+       01 Num2    pic 9 value zeroes.
+       01 Total   pic 99 value 0.
+       01 SSNum.
+              02 SSArea   pic 999.
+              02 SSGroup  pic 99.
+              02 SSSerial pic 9999.
+       01 PIValue constant as 3.14
+       
+       procedure division.
+       

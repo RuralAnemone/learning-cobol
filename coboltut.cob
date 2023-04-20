fb2922f96effff2b75a43497b330d942c238d41e
richardsonp <richardsonp@richardsonp.local>
2023-04-20 08:51:14 -0500

    fix comment

--- coboltut.cob
+++ coboltut.cob
@@ -8,8 +8,8 @@
 000800 data division.
 000900 file section.
 001000*
-001100* probably just gonna get familiar with the
-001150* syntax highlighting for now lol
+001100*probably just gonna get familiar with the
+001150*syntax highlighting for now lol
 001200 working-storage section.
 001300 01 UserName    pic x(30) value "user0".
 001400 01 Num1    pic 9 value zeroes.
@@ -20,12 +20,12 @@
 001900     02 SSGroup  pic 99.
 002000     02 SSSerial pic 9999.
 002100 01 PIValue constant as 3.14.
-002200* figurative (predefined?) constants:
-002300* -----------------------------------
-002400* zero, zeroes, zeros
-002500* space(s)
-002600* high-value(s) (largest value of defined type)
-002700* low-value(s) (smallest value of defined type)
+002200*figurative (predefined?) constants:
+002300*-----------------------------------
+002400*zero, zeroes, zeros
+002500*space(s)
+002600*high-value(s) (largest value of defined type)
+002700*low-value(s) (smallest value of defined type)
 002800*
 002900 procedure division.
 003000 display "enter your name: " with no advancing.
